c11311b910e400537228bfe2f9eb677d2c275c1c
RendyAB <114586224+RendyAB@users.noreply.github.com>
2025-02-24 20:44:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -19,7 +8,18 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'dist'
-    }
 });
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+//     build: {
+//         outDir: 'dist'
+//     }
+// });
